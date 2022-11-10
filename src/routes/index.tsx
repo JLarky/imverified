972923b94d89f40ef0533d61d18797a095bfa048
JLarky <jlarky@gmail.com>
2022-11-10 11:15:16 -0700

    credits

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,17 @@ export default function Home() {
 			<h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
 				We are taking seed money at this point
 			</h1>
+
+			<div class="mx-auto sm:max-w-[500px]">
+				<iframe
+					class="w-full sm:w-[500px] h-[300px] sm:h-[500px]"
+					src="https://www.youtube.com/embed/UAj1NXaB4lc?start=46"
+					title="YouTube video player"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen
+				></iframe>
+			</div>
 		</main>
 	);
 }
