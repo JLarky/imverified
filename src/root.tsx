0a5e271835cafeb893bd27f7b7d071a39dea631f
JLarky <jlarky@gmail.com>
2022-11-10 12:57:11 -0700

    solid start generator

--- src/root.tsx
+++ src/root.tsx
@@ -21,6 +21,7 @@ export default function Root() {
 				<Title>Bitch I'm Verified</Title>
 				<Meta charset="utf-8" />
 				<Meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta name="generator" content="SolidStart" />
 			</Head>
 			<Body>
 				<Suspense>
