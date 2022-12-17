c3ffa228f66b9f859fbcfdf824caf2a2825d3b98
JLarky <jlarky@gmail.com>
2022-12-17 15:13:02 -0700

    parasocial

--- src/routes/users/[username].tsx
+++ src/routes/users/[username].tsx
@@ -35,6 +35,23 @@ const fetchData = async (username: string) => {
     };
     return person;
   }
+  if (username === "parasocialfix") {
+    const url = `https://imverified.up.railway.app/users/${username}`;
+    const person: Person = {
+      verfiied: true,
+      name: "Parasoical Fix",
+      imageUrl:
+        "https://pbs.twimg.com/profile_images/1476597017941987328/JSQntEa-_400x400.jpg",
+      social: {
+        twitter: "https://twitter.com/parasocialfix",
+      },
+      url,
+      qrCode: await QRCode.toDataURL(url, {
+        color: { dark: "#1d9bf0", light: "#fff" },
+      }),
+    };
+    return person;
+  }
   return { verfiied: false as const };
 };
 
@@ -147,26 +164,28 @@ function Verified(props: { person: Person }) {
                         </svg>
                       </a>
                     </li>
-                    <li>
-                      <a
-                        href={person.social.linkedin}
-                        class="text-gray-400 hover:text-gray-500"
-                      >
-                        <span class="sr-only">LinkedIn</span>
-                        <svg
-                          class="h-5 w-5"
-                          aria-hidden="true"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
+                    {person.social.linkedin && (
+                      <li>
+                        <a
+                          href={person.social.linkedin}
+                          class="text-gray-400 hover:text-gray-500"
                         >
-                          <path
-                            fill-rule="evenodd"
-                            d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
-                      </a>
-                    </li>
+                          <span class="sr-only">LinkedIn</span>
+                          <svg
+                            class="h-5 w-5"
+                            aria-hidden="true"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                        </a>
+                      </li>
+                    )}
                   </ul>
                 </div>
               </div>
