d30e6d490031218988557ca54c0f94f468465c1b
JLarky <jlarky@gmail.com>
2023-01-02 11:33:58 -0700

    fix

--- src/root.tsx
+++ src/root.tsx
@@ -18,7 +18,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>Bitch I'm Verified</Title>
+        <Title>I'm Verified</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta name="generator" content="SolidStart" />
